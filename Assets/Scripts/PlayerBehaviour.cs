ec5b26560214387c7bbdbc6e4d16662d9ea64843
Evg2021 <83189739+Evg2021@users.noreply.github.com>
2022-04-15 17:13:55 +0300

    5 часов enum PlayerAction, jump- ForceMode.Impulse, LayerMask, IsGrounded, метод CheckCapsule() класса Physics, е QueryTriggerInteraction.Ignore

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,49 +7,65 @@ public class PlayerBehaviour : MonoBehaviour
     
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
 
-    
+    public float distanceToGround = 0.1f;
+
+    public LayerMask groundLayer;
+
     private float vInput;
     private float hInput;
-
-    //1 
     private Rigidbody _rb;
 
-    // Start is called before the first frame update
-    //2
+    private CapsuleCollider _col;
+    
     void Start()
     {
-        //3
+        
         _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        /*4
+        /*
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);         
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
         */
     }
-    //1
+    
     void FixedUpdate()
     {
-        //2
-        Vector3 rotation = Vector3.up * hInput;
 
-        //3
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+
+        Vector3 rotation = Vector3.up * hInput;
+                
         Quaternion angleRot = Quaternion.Euler(rotation * 
             Time.fixedDeltaTime);
-
-        //4 
+                
         _rb.MovePosition(this.transform.position + 
             this.transform.forward * vInput * Time.fixedDeltaTime);
-
-        //5
+                
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottm = new Vector3(_col.bounds.center.x, 
+            _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, 
+            capsuleBottm, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
 }
